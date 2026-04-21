ae77386e800915e6ef94c717aa343edf1aa723a5
Софьин Роман <rsofin@softrust.ru>
2026-04-21 17:28:55 +0500

    объединил вывод  внешних IP инстансов в единый map

--- 241231-Kafka/Normal/terraform/outputs.tf
+++ 241231-Kafka/Normal/terraform/outputs.tf
@@ -1,13 +1,8 @@
 # outputs.tf
 
-output "kafka01_external_ipv4" {
-  value = yandex_compute_instance.instances["kafka01"].network_interface[0].nat_ip_address
-}
-
-output "web01_external_ipv4" {
-  value = yandex_compute_instance.instances["web01"].network_interface[0].nat_ip_address
-}
-
-output "db01_external_ipv4" {
-  value = yandex_compute_instance.instances["db01"].network_interface[0].nat_ip_address
+output "instances_external_ipv4" {
+  value = {
+    for name, inst in yandex_compute_instance.instances :
+    name => inst.network_interface[0].nat_ip_address
+  }
 }
